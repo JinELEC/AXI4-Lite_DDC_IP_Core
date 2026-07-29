f8a92d5b7d2f476b17aafbf38b9d7b62312cb38a
Jinkyu Lee <ljkrhs3025@gmail.com>
2026-07-29 21:13:06 +0900

    Delete docs/fir_coefficients.m

--- docs/fir_coefficients.m
+++
@@ -1,8 +0,0 @@
-% 16-tap filter coefficients
-disp(sprintf('%d,',round(fir1(15,0.04)*32768)))
-
-% 32-tap filter coefficients
-disp(sprintf('%d,',round(fir1(31,0.04)*2147483648)))
-
-% 64-tap filter coefficients
-disp(sprintf('%d,',round(fir1(63,0.04)*9.223372e+18)))
