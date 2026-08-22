0ffe7e9560b1610e17c5453ae075e2f49c8617fe
Jinkyu Lee <ljkrhs3025@gmail.com>
2026-08-22 19:14:34 +0900

    Update adc_samples.m

--- matlab/adc_samples.m
+++ matlab/adc_samples.m
@@ -1,11 +1,9 @@
-Fs = 12.5e6;         % 100 MHz / 8
-
-f_in = 3.0e6;        % 3 MHz input IF signal
-
+Fs = 100e6 / 20;      % 100 MHz / 20
+f_in = 1.466667e6;    % 1.4667 MHz Input IF (f_LO=1.6667MHz, translated=0.2MHz)
 N = 1024;
-
 t = (0:N-1) / Fs;
 
+% 8-bit Signed ADC data (-128 ~ 127)
 adc_data = round(127 * cos(2*pi*f_in*t));
 
 writematrix(adc_data', 'adc_samples.txt');
