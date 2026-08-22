38d61ac299dd366d1e06a58afe2e047b265016da
Jinkyu Lee <ljkrhs3025@gmail.com>
2026-08-22 19:15:19 +0900

    Update fir_coefficients.m

--- matlab/fir_coefficients.m
+++ matlab/fir_coefficients.m
@@ -1,8 +1,8 @@
 % 16-tap filter coefficients
-disp(sprintf('%d,',round(fir1(15,0.10)*32768)))
+disp(sprintf('%d,',round(fir1(15,0.25)*32768)))
 
 % 32-tap filter coefficients
-disp(sprintf('%d,',round(fir1(31,0.10)*32768)))
+disp(sprintf('%d,',round(fir1(31,0.25)*32768)))
 
 % 64-tap filter coefficients
-disp(sprintf('%d,',round(fir1(63,0.10)*32768)))
+disp(sprintf('%d,',round(fir1(63,0.25)*32768)))
